b47d7fefc0d0c960fc9c8e26fc27c661d2e93bd2
itskevinParker <abid.shahriar135@gmail.com>
2026-07-21 11:34:00 +0600

    Implement singleton print spooler with job processing

--- Task 8/Main6.java
+++ Task 8/Main6.java
@@ -1,1 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class PrintSpooler {
+    private static volatile PrintSpooler instance;
+    private Queue<String> printQueue;
+
+    private PrintSpooler() {
+        printQueue = new LinkedList<>();
+    }
+
+    public static PrintSpooler getInstance() {
+        if (instance == null) {
+            synchronized (PrintSpooler.class) {
+                if (instance == null) {
+                    instance = new PrintSpooler();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void addJob(String document) {
+        printQueue.add(document);
+        System.out.println("Added to global queue: " + document);
+    }
+
+    public void processJobs() {
+        System.out.println("--- Processing Unified Queue ---");
+        while (!printQueue.isEmpty()) {
+            System.out.println("Printing: " + printQueue.poll());
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+        PrintSpooler.getInstance().addJob("Lab_Manual.pdf");
+        
+        
+        PrintSpooler.getInstance().addJob("Grades.xlsx");
+
+        
+        PrintSpooler.getInstance().processJobs();
+    }
+}
 
